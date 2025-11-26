7cd8143dc0130d4a1af3c260abb238f02d0ddbec
Jsesam <164118703+Jsesam@users.noreply.github.com>
2025-11-26 16:09:41 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let index = 0;
+const slides = document.querySelectorAll(".slide");
+
+function cambiarSlide() {
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+    slides[index].classList.add("active");
+}
+
+setInterval(cambiarSlide, 2000);
